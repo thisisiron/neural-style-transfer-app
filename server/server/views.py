a45eef076e128f589d238fee6647f84d82dce711
thisisiron <kimiron518@gmail.com>
2018-10-30 23:26:07 +0900

    send the image converted to Gogh to Android

--- server/server/views.py
+++ server/server/views.py
@@ -3,15 +3,26 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.shortcuts import render
 import os
+from PIL  import Image
+import re
 
 def index(request):
-    return HttpResponse("Derp")
+    return HttpResponse("Hello World")
 
 @csrf_exempt
 def upload_file(request):
     if 'pic' in request.FILES:
         ret = handle_uploaded_file(request.FILES['pic'])
-        return HttpResponse("Uploaded Image!" + "\n" + ret)
+        fname = re.findall(r'server/media/(.*).jpg', ret)[0]
+        try:
+            with open('./transfer/' + fname + '_transfer.jpg', "rb") as f:
+                return HttpResponse(f.read(), content_type="image/jpeg")
+        except IOError:
+            red = Image.new('RGBA', (1, 1), (255,0,0,0))
+            response = HttpResponse(content_type="image/jpeg")
+            red.save(response, "JPEG")
+            return response
+        # return HttpResponse("Uploaded Image!" + "\n" + ret)
     return HttpResponse("No Image Received!")
 
 def handle_uploaded_file(f):
@@ -24,5 +35,4 @@ def handle_uploaded_file(f):
     return fname
 
 def transfer(fname):
-    print(fname)
     os.system('python neural_style_transfer.py --image ' + fname + ' --model models/eccv16/starry_night.t7')
